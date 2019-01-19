a8e11442e6ddd6f9cd75d08cf0aba12ba217450c
bssrdf <merlintiger@hotmail.com>
2019-01-18 22:46:43 -0500

    make char** point to 2D array to keep interface working

--- 0200-Number-of-Islands/c-0200/main.c
+++ 0200-Number-of-Islands/c-0200/main.c
@@ -3,6 +3,7 @@
 /// Time   : 2017-11-18
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 
@@ -12,31 +13,22 @@
 /// Time Complexity: O(n*m)
 /// Space Complexity: O(n*m)
 
-bool visit(void *grid0, int gridRowSize, int gridColSize, int x, int y){
-
-    char (*grid)[gridRowSize] = grid0;
-    printf(" in visit, i, j, c = (%d, %d, %c) \n", x, y, grid[0][0]);
+bool visit(char **grid, int gridRowSize, int gridColSize, int x, int y){
     if(x < 0 || x >= gridRowSize || y < 0 || y >= gridColSize || grid[x][y] != '1')
-    //if(x < 0 || x >= gridRowSize || y < 0 || y >= gridColSize)
 	   return false;
-    printf(" in visit after bound check, i, j = (%d, %d) \n", x, y);
-	grid[x][y] = 'v';   
-	visit(grid, gridRowSize, gridColSize, x+1, y);
-	visit(grid, gridRowSize, gridColSize, x-1, y);
-	visit(grid, gridRowSize, gridColSize, x, y+1);
-	visit(grid, gridRowSize, gridColSize, x, y-1);
-	return true;
+    grid[x][y] = 'v';   
+    visit(grid, gridRowSize, gridColSize, x+1, y);
+    visit(grid, gridRowSize, gridColSize, x-1, y);
+    visit(grid, gridRowSize, gridColSize, x, y+1);
+    visit(grid, gridRowSize, gridColSize, x, y-1);
+    return true;
 }
 
-int numIslands(void* grid0, int gridRowSize, int gridColSize) {
-//int numIslands(char** grid, int gridRowSize, int gridColSize) {
-        char (*grid)[gridRowSize] = grid0;
+int numIslands(char** grid, int gridRowSize, int gridColSize) {
         int res = 0;
         for(int i = 0 ; i < gridRowSize ; i ++)
-            for(int j = 0 ; j < gridColSize; j ++){
-			   printf(" i,j,c = (%d, %d, %c) \n", i, j, grid[i][j]); 
-			   res += visit((char**)grid, gridRowSize, gridColSize, i, j)? 1 : 0;
-			}
+            for(int j = 0 ; j < gridColSize; j ++)
+		 res += visit(grid, gridRowSize, gridColSize, i, j)? 1 : 0;
         return res;
 }
 
@@ -50,9 +42,12 @@ int main() {
             {'0','0','0','0','0'}
     };
 
-	printf(" %c \n", g1[0][0]);
-	//printf(" %d \n", numIslands((void*)g1, 4, 5));
-	printf(" %d \n", numIslands(g1, 4, 5));
+    char** grid;
+    grid = (char**)malloc(4*sizeof(char*));
+    for(int i = 0; i < 4; ++i)
+        grid[i] = g1[i];
+
+    printf(" %d \n", numIslands(grid, 4, 5));
     // 1
 
     // ---
@@ -63,9 +58,12 @@ int main() {
             {'0','0','1','0','0'},
             {'0','0','0','1','1'}
     };
+    for(int i = 0; i < 4; ++i)
+        grid[i] = g2[i];
 
-	printf(" %d \n", numIslands((char**)g2, 4, 5));
+	printf(" %d \n", numIslands(grid, 4, 5));
     // 2
 
+     free(grid);
     return 0;
 }

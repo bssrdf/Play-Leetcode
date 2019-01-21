e22592f2f4b203d16bf6524fef0fae331bf5b1b8
bssrdf <bssrdf@gmail.com>
2019-01-21 02:34:27 -0500

    fixed crash by changing auto vars to heap vars for queue node

--- 0200-Number-of-Islands/c-0200/main2.c
+++ 0200-Number-of-Islands/c-0200/main2.c
@@ -29,23 +29,23 @@ bool visit(char **grid, int gridRowSize, int gridColSize, int x, int y){
     Queue q;
 	queue_init(&q);
     grid[x][y] = 'v';  
-	Node a; a.i = x; a.j = y;
-	queue_push(&q, &a.node);
+    Node *a = (Node*)malloc(sizeof(Node));
+    a->i = x; a->j = y;
+	queue_push(&q, &(a->node));
 	while(!queue_empty(&q)){
 	  int i=queue_entry(queue_peek(&q), Node, node)->i;
 	  int j=queue_entry(queue_peek(&q), Node, node)->j;
-	  printf(" (i,j, qsize) = (%d, %d, %u) \n", i,j,queue_size(&q));
+	  free(queue_entry(queue_pop(&q), Node, node));
 	  for (int k=0; k<4; ++k){
 	     int ii = i+incx[k]; int jj = j+incy[k];
          if(ii>=0 && ii<gridRowSize && jj>=0 && jj<gridColSize && grid[ii][jj] == '1'){
-	         printf(" (k,i,j, qsize) = (%d, %d, %d, %u) \n", k, i,j,queue_size(&q));
+	//         printf(" (k,ii,jj, qsize) = (%d, %d, %d, %zu) \n", k, ii, jj, queue_size(&q));
 		     grid[ii][jj] = 'v';
-		     Node b; b.i = ii; b.j = jj;
-	         queue_push(&q, &b.node);
+		     Node *b = (Node*)malloc(sizeof(Node));
+			 b->i = ii; b->j = jj;
+	         queue_push(&q, &(b->node));
 		}
 	  }
-	  queue_pop(&q);
-	  printf(" (qsize) = (%u) \n", queue_size(&q));
 	}
     return true;
 }
